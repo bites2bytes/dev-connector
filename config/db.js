74c0cd8f562ad75a8841478a9ed1880a967619a2
Monika Sazdova <59760777+MonikaSazdova@users.noreply.github.com>
2021-03-07 22:44:23 +0100

    All routes on backend added

--- config/db.js
+++ config/db.js
@@ -8,7 +8,8 @@ const connectDB = async () => {
 			{
 				useNewUrlParser: true,
 				useUnifiedTopology: true,
-				useCreateIndex: true
+				useCreateIndex: true,
+				useFindAndModify: false
 			})
 		console.log("MongoDB Connected")
 	} catch (err) {
--- models/Profile.js
+++ models/Profile.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const ProfileSchema = new mongoose.Schema({
 	user: {
 		type: mongoose.Schema.Types.ObjectId,
-		ref: 'users'
+		ref: 'user'
 	},
 	company: {
 		type: String
